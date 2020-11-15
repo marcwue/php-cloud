a96804160375e4bf2d999cec35574bf59adb0d1e
marc <marcwu13@gmail.com>
2020-11-15 14:25:36 +0100

    php to version 7.3 and added more header

--- src/api.php
+++ src/api.php
@@ -1,4 +1,8 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header("Content-Type: application/json; charset=UTF-8");
+
 include 'crawler/crawlerController.php';
 
 error_log("api endpoint start", 0);
@@ -14,7 +18,7 @@ if ($website !== "" && substr($website, 0, strlen($http)) === $http) {
   echo "Enter a correct webiste starting with http://...";
 }
 
-header("Content-Type:application/json");
+error_log("viewModel=" . print_r($siteContentViewModel, true), 0);
 error_log("api endpoint end", 0);
 echo $siteContentViewModel;
 ?>
